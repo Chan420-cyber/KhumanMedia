6d05201f34f604b4a999d0e8e61635e1c72cf96e
Chan420-cyber <ashimrayahor@gmail.com>
2025-05-10 23:06:12 +0530

    arrange

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const characters = [
   {
     name: "THE EXCUSE",
     text: "Just a regular student life – chilling with friends over coffee after tuition, coming home late, complaining about dinner, then heading to school only to hear there’s a test. Suddenly ‘falling sick’ to dodge it  –  only to return and get ambushed by the postponed test anyway. No escape!",
-    image: "assets/background.jpg"
+    image: "assets/1(TheExcuse)Page1 - Copy.jpg"
   },
   {
     name: "THE RED ZONE",
     text: "Two friends hype themselves up to meet a girl one of them matched with on Facebook—so one even fibs to his mom about ‘work.’ When they roll up, they find the spot is a red-zone guarded by a squad of women and beat a hasty retreat. Dejected, they stop for some bora, only to have their spirits lifted by the sight of a stunning server. Problem solved!",
-    image: "assets/2.jpg"
+    image: "assets/2(TheRedZone)Page1 - Copy.jpg"
   },
   {
     name: "HANUBA HANUBI PAN THABA",
     text: "An old couple heads to the field to plant yams, chatting about mischievous monkeys who might dig them up. Just then, some monkeys show up and surprisingly offer farming advice—boil the yam, bury it with a big leaf, and it’ll grow overnight. The couple tries it, and by the next day, the yam has shot up tall and strong. To be continued...",
-    image: "assets/3.jpg"
+    image: "assets/5(Hanuba-Hanubi-pan-thaba)Page1 - Copy.jpg"
   },
   {
     name: "THE EXCUSE EPISODE 2",
     text: "Inosuke is wild, fearless, and wears a boar’s head—perfect for charging into battle.",
-    image: "assets/4.jpg"
+    image: "assets/4(TheExcuseEpi2)Page1 - Copy.jpg"
   }
 ];
 
